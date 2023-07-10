2adb3518c3425416ecf485acb72942ec1ae0e99e
Ikechukwu Anachebe <anachebeikechukwu68@gmail.com>
2023-07-10 10:12:35 -0700

    Base model created

---
+++ models/base_model.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""This module imports two foreign functions and contains a class that uses
+   them.
+"""
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel():
+    """This class is the foundation of a great program.
+    """
+
+    def __init__(self):
+        """This method initantializes three public instance attributes.
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """This is the __str__ method.
+
+        Return:
+            str: Information on the class.
+        """
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """This method updates the time of ``updated_at`` when it is called.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """This method creates a dictionary of the this class and it's
+           instance attributes.
+
+        Return:
+            dict: A dictionary containing the instance attributes of this
+            class.
+        """
+        my_dict = {}
+
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                value = value.isoformat()
+
+            my_dict.update({key: value})
+
+        my_dict.update({"__class__": f"{__class__.__name__}"})
+        return my_dict
